a898b54d268782290ccf26fe761f39fe203d7ead
Brandon Dunne <bdunne@redhat.com>
2013-04-26 16:11:06 -0400

    Adding tests for Yum commands

---
+++ spec/yum_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe LinuxAdmin::Yum do
+  context ".updates_available?" do
+    it "updates are available" do
+      LinuxAdmin::Common.stub(:run => 100)
+      expect(described_class.updates_available?).to be_true
+    end
+
+    it "updates not available" do
+      LinuxAdmin::Common.stub(:run => 0)
+      expect(described_class.updates_available?).to be_false
+    end
+
+    it "other exit code" do
+      LinuxAdmin::Common.stub(:run => 255)
+      expect { described_class.updates_available? }.to raise_error
+    end
+
+    it "other error" do
+      LinuxAdmin::Common.stub(:run).and_raise(RuntimeError)
+      expect { described_class.updates_available? }.to raise_error
+    end
+  end
+
+  it ".update" do
+    LinuxAdmin::Common.should_receive(:run).once
+    described_class.update
+  end
+end
