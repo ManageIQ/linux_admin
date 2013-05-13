ecd5fc35e566d2255a40d055b6bae520e102044a
Brandon Dunne <bdunne@redhat.com>
2013-05-13 11:27:13 -0400

    Add parsing of Yum repo files with tests and sample data.

--- lib/linux_admin/yum.rb
+++ lib/linux_admin/yum.rb
@@ -1,30 +1,41 @@
+require 'fileutils'
+require 'inifile'
+
 module LinuxAdmin
   module Yum
     def self.create_repo(path, options={})
-      Common.run("mkdir -p #{path}")
+      raise ArgumentError, "path is required" unless path
 
-      create_cmd = "yum createrepo #{path}"
-      create_cmd << " --database" unless options[:no_database]
-      create_cmd << " --unique-md-filenames" unless options[:no_unique_file_names]
-      Common.run(create_cmd)
+      FileUtils.mkdir_p(LinuxAdmin::Common.sanitize(path))
+
+      cmd = ["yum createrepo", path]
+      cmd.push(" --database")             unless options[:no_database]
+      cmd.push(" --unique-md-filenames")  unless options[:no_unique_file_names]
+      Common.run(LinuxAdmin::Common.sanitize(cmd))
     end
 
-    def self.download_packages(path, package_string, options={})
-      Common.run("mkdir -p #{path}")
+    def self.download_packages(path, packages, options={:mirror_type => :package})
+      raise ArgumentError, "path is required"       unless path
+      raise ArgumentError, "packages are required"  unless packages
 
-      case options[:mirror_type]
-      when :package; mirror_cmd = "yum repotrack -p #{path}"
-      else; raise "mirror_type required"
-      end
-      mirror_cmd << " -a #{options[:arch]}" if options[:arch]
-      mirror_cmd << " #{package_string}.strip"
-      Common.run(mirror_cmd)
+      FileUtils.mkdir_p(LinuxAdmin::Common.sanitize(path))
+
+      cmd = case options[:mirror_type]
+            when :package;  ["yum repotrack -p", path]
+            else;           raise ArgumentError, "mirror_type unsupported"
+            end
+      cmd.push("-a", options[:arch]) if options[:arch]
+      cmd.push(packages)
+      Common.run(LinuxAdmin::Common.sanitize(cmd))
     end
 
-    def self.updates_available?(*pkgs)
-      cmd = "yum check-update"
-      cmd << " #{pkgs.join(" ").strip}" unless pkgs.empty?
-      exitstatus = Common.run(cmd, :return_exitstatus => true)
+    def self.repo_settings
+      self.parse_repo_dir("/etc/yum.repos.d")
+    end
+
+    def self.updates_available?(*packages)
+      cmd = ["yum check-update", packages]
+      exitstatus = Common.run(LinuxAdmin::Common.sanitize(cmd), :return_exitstatus => true)
       case exitstatus
       when 0;   false
       when 100; true
@@ -35,6 +46,22 @@ module LinuxAdmin
     def self.update(*packages)
       cmd = ["yum -y update", packages]
       Common.run(LinuxAdmin::Common.sanitize(cmd))
+    end
+
+    private
+
+    def self.parse_repo_dir(dir)
+      repo_files = Dir.glob(File.join(dir, '*.repo'))
+      repo_files.each_with_object({}) do |file, content|
+        content[file] = self.parse_repo_file(file)
+      end
+    end
+
+    def self.parse_repo_file(file)
+      content = IniFile.load(file).to_h
+      content.each do |name, data|
+        data.each { |k, v|  data[k] = v.to_i if "enabled, cost, gpgcheck, sslverify, metadata_expire".include?(k)}
+      end
     end
   end
 end
--- spec/yum_spec.rb
+++ spec/yum_spec.rb
@@ -1,21 +1,69 @@
 require 'spec_helper'
 
 describe LinuxAdmin::Yum do
-  it ".create_repo" do
-    LinuxAdmin::Common.stub(:run => true)
-    expect(described_class.create_repo("some_path")).to be_true
+  before(:each) do
+    FileUtils.stub(:mkdir_p => true)
+  end
+
+  context ".create_repo" do
+    it "default arguments" do
+      LinuxAdmin::Common.should_receive(:run).once.with("yum createrepo some/path --database --unique-md-filenames").and_return true
+      expect(described_class.create_repo("some/path")).to be_true
+    end
+
+    it "bare create" do
+      LinuxAdmin::Common.should_receive(:run).once.with("yum createrepo some/path").and_return true
+      expect(described_class.create_repo("some/path", :no_database => true, :no_unique_file_names => true)).to be_true
+    end
   end
 
   context ".download_packages" do
     it "with valid input" do
-      LinuxAdmin::Common.stub(:run => true)
-      expect(described_class.download_packages("some_path", "pkg_a pkg_b", :mirror_type => :package)).to be_true
+      LinuxAdmin::Common.should_receive(:run).once.with("yum repotrack -p some/path pkg_a pkg_b").and_return true
+      expect(described_class.download_packages("some/path", "pkg_a pkg_b")).to be_true
     end
 
     it "without mirror type" do
-      LinuxAdmin::Common.stub(:run => true)
-      expect { described_class.download_packages("some_path", "pkg_a pkg_b") }.to raise_error
+      expect { described_class.download_packages("some/path", "pkg_a pkg_b", :mirror_type => nil) }.to raise_error(ArgumentError)
     end
+  end
+
+  it ".repo_settings" do
+    described_class.should_receive(:parse_repo_dir).once.with("/etc/yum.repos.d").and_return(true)
+    expect(described_class.repo_settings).to be_true
+  end
+
+  it ".parse_repo_dir" do
+    expect(described_class.parse_repo_dir(data_file_path("yum"))).to eq({
+      File.join(data_file_path("yum"), "first.repo")  =>
+        { "my-local-repo-a"   =>
+          { "name"            =>"My Local Repo A",
+            "baseurl"         =>"https://mirror.example.com/a/content/os_ver",
+            "enabled"         =>0,
+            "gpgcheck"        =>1,
+            "gpgkey"          =>"file:///etc/pki/rpm-gpg/RPM-GPG-KEY-my-local-server",
+            "sslverify"       =>1,
+            "sslcacert"       =>"/etc/rhsm/ca/my-loacl-server.pem",
+            "sslclientkey"    =>"/etc/pki/entitlement/0123456789012345678-key.pem",
+            "sslclientcert"   =>"/etc/pki/entitlement/0123456789012345678.pem",
+            "metadata_expire" =>86400},
+          "my-local-repo-b" =>
+          { "name"            =>"My Local Repo B",
+            "baseurl"         =>"https://mirror.example.com/b/content/os_ver",
+            "enabled"         =>1,
+            "gpgcheck"        =>0,
+            "sslverify"       =>0,
+            "metadata_expire" =>86400}},
+      File.join(data_file_path("yum"), "second.repo") =>
+        { "my-local-repo-c" =>
+          { "name"            =>"My Local Repo c",
+            "baseurl"         =>"https://mirror.example.com/c/content/os_ver",
+            "enabled"         =>0,
+            "cost"            =>100,
+            "gpgcheck"        =>1,
+            "gpgkey"          =>"file:///etc/pki/rpm-gpg/RPM-GPG-KEY-my-local-server",
+            "sslverify"       =>0,
+            "metadata_expire" =>1}},})
   end
 
   context ".updates_available?" do
