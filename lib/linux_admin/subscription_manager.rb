0902e5eae01f8ac5c9576d5a94fda0e01218e83e
Brandon Dunne <bdunne@redhat.com>
2013-06-25 15:04:27 -0400

    Add proxy support to subscribe, refactor proxy details into a method.
    Make register and subscribe options required.
    Allow for multiple pools to be specified.

--- lib/linux_admin/subscription_manager.rb
+++ lib/linux_admin/subscription_manager.rb
@@ -10,28 +10,24 @@ class LinuxAdmin
       run("subscription-manager refresh")
     end
 
-    def self.register(options = {})
+    def self.register(options)
       raise ArgumentError, "username and password are required" unless options[:username] && options[:password]
       cmd = "subscription-manager register"
 
-      params = {}
-      if options[:username] && options[:password]
-        params["--username="]     = options[:username]
-        params["--password="]     = options[:password]
-      end
-      params["--org="]            = options[:org]             if options[:org] && options[:server_url]
-      params["--proxy="]          = options[:proxy_address]   if options[:proxy_address]
-      params["--proxyuser="]      = options[:proxy_username]  if options[:proxy_username]
-      params["--proxypassword="]  = options[:proxy_password]  if options[:proxy_password]
-      params["--serverurl="]      = options[:server_url]      if options[:server_url]
+      params = {"--username=" => options[:username], "--password=" => options[:password]}
+      params.merge!(proxy_params(options))
+      params["--org="]        = options[:org]         if options[:server_url] && options[:org]
+      params["--serverurl="]  = options[:server_url]  if options[:server_url]
 
       run(cmd, :params => params)
     end
 
-    def self.subscribe(pool_id)
-      params = {"--pool" => pool_id}
+    def self.subscribe(options)
+      cmd    = "subscription-manager attach"
+      pools  = options[:pools].collect {|pool| ["--pool", pool]}
+      params = proxy_params(options).to_a + pools
 
-      run("subscription-manager attach", :params => params)
+      run(cmd, :params => params)
     end
 
     def self.available_subscriptions
@@ -50,6 +46,16 @@ class LinuxAdmin
 
         subscriptions_hash[hash[:pool_id]] = hash
       end
+    end
+
+    private
+
+    def self.proxy_params(options)
+      config = {}
+      config["--proxy="]          = options[:proxy_address]   if options[:proxy_address]
+      config["--proxyuser="]      = options[:proxy_username]  if options[:proxy_username]
+      config["--proxypassword="]  = options[:proxy_password]  if options[:proxy_password]
+      config
     end
   end
 end
