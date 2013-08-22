6862d72b31f22b355ad842434e25bf7a1c012ac6
Mo Morsi <mmorsi@redhat.com>
2013-08-22 13:09:43 -0400

    introduce helper method to reduce code duplication

---
+++ lib/linux_admin/volume.rb
@@ -0,0 +1,33 @@
+# LinuxAdmin Logical Volume Representation
+#
+# Copyright (C) 2013 Red Hat Inc.
+# Licensed under the MIT License
+
+class LinuxAdmin
+  class Volume < LinuxAdmin
+    private
+
+    def self.process_volume_display_line(line)
+      groups = VolumeGroup.scan
+      fields = line.split(':')
+      vgname = fields[1] 
+      vg = groups.find { |vg| vg.name == vgname }
+      return fields, vg 
+    end
+
+    protected
+
+    def self.scan_volumes(cmd)
+      volumes = []
+
+      out = run!(cmd, :params => { '-c' => nil}).output
+
+      out.each_line do |line|
+        fields, vg = process_volume_display_line(line)
+        volumes << yield(fields, vg)
+      end
+
+      volumes
+    end
+  end
+end
