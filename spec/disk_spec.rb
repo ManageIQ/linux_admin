4602d13959aa010a3c2d102e35ff3eef97588a19
Nick Carboni <ncarboni@redhat.com>
2015-07-14 11:38:33 -0400

    Fixed NumericLiteral cop in disk spec

--- spec/disk_spec.rb
+++ spec/disk_spec.rb
@@ -37,7 +37,7 @@ eos
 
       disk = LinuxAdmin::Disk.new :path => '/dev/hda'
       allow(disk).to receive(:run!).and_return(double(:output => fdisk))
-      expect(disk.size).to eq(500107862016)
+      expect(disk.size).to eq(500_107_862_016)
     end
   end
 
