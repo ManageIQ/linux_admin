88a2a3c1d98e880bf19a91f29069675c7207e743
Nick Carboni <ncarboni@redhat.com>
2015-07-23 16:00:35 -0400

    Added a systemctl class to the linux admin gem
    As init has been depricated it is quickly becoming necessary
    to use systemctl in its place.

---
+++ lib/linux_admin/systemctl.rb
@@ -0,0 +1,57 @@
+module LinuxAdmin
+  class Systemctl
+    include Common
+
+    attr_accessor :id
+    private
+
+    public
+
+    def initialize(id)
+      @id = id
+    end
+
+    def running?
+      run(cmd(:systemctl),
+          :params => { nil => ["status", id] }).exit_status == 0
+    end
+
+    def enable
+      run!(cmd(:systemctl),
+          :params => { nil => ["enable", id] })
+      self
+    end
+
+    def disable
+      run!(cmd(:systemctl),
+          :params => { nil => ["disable", id] })
+      self
+    end
+
+    def start
+      run!(cmd(:systemctl),
+          :params => { nil => ["start", id] })
+      self
+    end
+
+    def stop
+      run!(cmd(:systemctl),
+          :params => { nil => ["stop", id] })
+      self
+    end
+
+    def restart
+      status =
+        run(cmd(:systemctl),
+          :params => { nil => ["restart", id] }).exit_status
+
+      # attempt to manually stop/start if restart fails
+      if status != 0
+        self.stop
+        self.start
+      end
+
+      self
+    end
+  end
+end
