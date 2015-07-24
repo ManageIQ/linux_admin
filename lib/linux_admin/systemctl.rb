6894857d80f2df7c434d8fbf59c502dc493f36c0
Nick Carboni <ncarboni@redhat.com>
2015-07-24 14:31:07 -0400

    Changed service name from id to name in systemctl class

--- lib/linux_admin/systemctl.rb
+++ lib/linux_admin/systemctl.rb
@@ -2,47 +2,47 @@ module LinuxAdmin
   class Systemctl
     include Common
 
-    attr_accessor :id
+    attr_accessor :name
 
     public
 
-    def initialize(id)
-      @id = id
+    def initialize(name)
+      @name = name
     end
 
     def running?
       run(cmd(:systemctl),
-          :params => {nil => ["status", id]}).exit_status == 0
+          :params => {nil => ["status", name]}).exit_status == 0
     end
 
     def enable
       run!(cmd(:systemctl),
-           :params => {nil => ["enable", id]})
+           :params => {nil => ["enable", name]})
       self
     end
 
     def disable
       run!(cmd(:systemctl),
-           :params => {nil => ["disable", id]})
+           :params => {nil => ["disable", name]})
       self
     end
 
     def start
       run!(cmd(:systemctl),
-           :params => {nil => ["start", id]})
+           :params => {nil => ["start", name]})
       self
     end
 
     def stop
       run!(cmd(:systemctl),
-           :params => {nil => ["stop", id]})
+           :params => {nil => ["stop", name]})
       self
     end
 
     def restart
       status =
         run(cmd(:systemctl),
-            :params => {nil => ["restart", id]}).exit_status
+            :params => {nil => ["restart", name]}).exit_status
 
       # attempt to manually stop/start if restart fails
       if status != 0
