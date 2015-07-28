7a8798386df614d8cb9e1d9eea3ccdda7fd38835
Nick Carboni <ncarboni@redhat.com>
2015-07-28 09:29:54 -0400

    Fixed hash alignment rubocop issue

--- lib/linux_admin/security/sysctl_conf.rb
+++ lib/linux_admin/security/sysctl_conf.rb
@@ -4,17 +4,17 @@ module LinuxAdmin
       CONF_FILE = "/etc/sysctl.conf"
 
       SCAP_SETTINGS = {
-        "net.ipv4.conf.all.accept_redirects" => 0,
-        "net.ipv4.conf.all.secure_redirects" => 0,
-        "net.ipv4.conf.all.log_martians" => 1,
-        "net.ipv4.conf.default.secure_redirects" => 0,
-        "net.ipv4.conf.default.accept_redirects" => 0,
-        "net.ipv4.icmp_echo_ignore_broadcasts" => 1,
+        "net.ipv4.conf.all.accept_redirects"         => 0,
+        "net.ipv4.conf.all.secure_redirects"         => 0,
+        "net.ipv4.conf.all.log_martians"             => 1,
+        "net.ipv4.conf.default.secure_redirects"     => 0,
+        "net.ipv4.conf.default.accept_redirects"     => 0,
+        "net.ipv4.icmp_echo_ignore_broadcasts"       => 1,
         "net.ipv4.icmp_ignore_bogus_error_responses" => 1,
-        "net.ipv4.conf.all.rp_filter" => 1,
-        "net.ipv6.conf.default.accept_redirects" => 0,
-        "net.ipv4.conf.default.send_redirects" => 0,
-        "net.ipv4.conf.all.send_redirects" => 0
+        "net.ipv4.conf.all.rp_filter"                => 1,
+        "net.ipv6.conf.default.accept_redirects"     => 0,
+        "net.ipv4.conf.default.send_redirects"       => 0,
+        "net.ipv4.conf.all.send_redirects"           => 0
       }
 
       def self.apply_scap_settings(filename = CONF_FILE)
