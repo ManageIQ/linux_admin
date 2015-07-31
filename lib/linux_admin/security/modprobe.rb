9a2e5b7d421ce6c52620d0c1c2d6e82e750e842f
Nick Carboni <ncarboni@redhat.com>
2015-07-31 08:55:51 -0400

    Made conf file constant consistant across classes

--- lib/linux_admin/security/modprobe.rb
+++ lib/linux_admin/security/modprobe.rb
@@ -1,10 +1,10 @@
 module LinuxAdmin
   class Security
     class Modprobe
-      CONF_FILE_NAME = "/etc/modprobe.d/scap_lockdown"
+      CONF_FILE = "/etc/modprobe.d/scap_lockdown"
       SCAP_MODULES = %w(dccp sctp rds tipc)
 
-      def self.apply_scap_settings(filename = CONF_FILE_NAME)
+      def self.apply_scap_settings(filename = CONF_FILE)
         SCAP_MODULES.each do |m|
           disable_module(m, filename)
         end
