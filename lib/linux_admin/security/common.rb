c17840de0044c0ceef1e7765e71d3bf9a622875f
Nick Carboni <ncarboni@redhat.com>
2015-08-03 16:04:04 -0400

    Cleaned up Security::Common.replace_config_line

--- lib/linux_admin/security/common.rb
+++ lib/linux_admin/security/common.rb
@@ -3,11 +3,8 @@ module LinuxAdmin
     module Common
       def replace_config_line(new_line, rep_regex, file_text)
         new_text = file_text.gsub!(rep_regex, new_line)
-        if new_text
-          new_text
-        else
-          file_text << new_line
-        end
+        return new_text if new_text
+        file_text << new_line
       end
     end
   end
