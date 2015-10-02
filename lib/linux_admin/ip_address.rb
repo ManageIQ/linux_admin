cbd8f2abeff0a48a209bd3ecf6658dc000f8efba
Nick Carboni <ncarboni@redhat.com>
2015-10-02 16:18:46 -0400

    Added netmask method to IpAddress class

--- lib/linux_admin/ip_address.rb
+++ lib/linux_admin/ip_address.rb
@@ -14,12 +14,22 @@ module LinuxAdmin
       result = run(cmd("ip"), :params => ["addr", "show", interface])
       return nil if result.failure?
 
-      lines = result.output.split("\n")
-      link_line = lines.detect { |l| l =~ %r{link/ether} }
-      link_line.nil? ? nil : link_line.strip.split(' ')[1]
+      parse_output(result.output, %r{link/ether}, 1)
+    end
+
+    def netmask(interface)
+      result = run(cmd("ifconfig"), :params => [interface])
+      return nil if result.failure?
+
+      parse_output(result.output, /netmask/, 3)
     end
 
     private
+
+    def parse_output(output, regex, col)
+      the_line = output.split("\n").detect { |l| l =~ regex }
+      the_line.nil? ? nil : the_line.strip.split(' ')[col]
+    end
 
     def address_list
       result = nil
--- spec/ip_address_spec.rb
+++ spec/ip_address_spec.rb
@@ -11,6 +11,11 @@ describe LinuxAdmin::IpAddress do
     :params => %w(addr show eth0)
   ]
 
+  MASK_SPAWN_ARGS = [
+    described_class.new.cmd("ifconfig"),
+    :params => %w(eth0)
+  ]
+
   IP_ADDR_SHOW_ETH0 = <<-IP_OUT
 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
     link/ether 00:0c:29:ed:0e:8b brd ff:ff:ff:ff:ff:ff
@@ -21,12 +26,15 @@ describe LinuxAdmin::IpAddress do
 
 IP_OUT
 
-  IP_ADDR_ERR = <<-IP_OUT
-2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
-    inet 192.168.1.9/24 brd 192.168.1.255 scope global dynamic eth0
-       valid_lft 1297sec preferred_lft 1297sec
-    inet6 fe80::20c:29ff:feed:e8b/64 scope link
-       valid_lft forever preferred_lft forever
+  IFCFG = <<-IP_OUT
+eth0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+        inet 192.168.1.9  netmask 255.255.255.0  broadcast 192.168.1.255
+        inet6 fe80::20c:29ff:feed:e8b  prefixlen 64  scopeid 0x20<link>
+        ether 00:0c:29:ed:0e:8b  txqueuelen 1000  (Ethernet)
+        RX packets 10171  bytes 8163955 (7.7 MiB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 2871  bytes 321915 (314.3 KiB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
 IP_OUT
 
@@ -86,8 +94,27 @@ IP_OUT
     end
 
     it "returns nil if the link/ether line is not present" do
-      expect(AwesomeSpawn).to receive(:run).with(*MAC_SPAWN_ARGS).and_return(result(IP_ADDR_ERR, 0))
+      bad_output = IP_ADDR_SHOW_ETH0.gsub(%r{link/ether}, "")
+      expect(AwesomeSpawn).to receive(:run).with(*MAC_SPAWN_ARGS).and_return(result(bad_output, 0))
       expect(ip.mac_address("eth0")).to be_nil
+    end
+  end
+
+  describe "#netmask" do
+    it "returns the correct netmask" do
+      expect(AwesomeSpawn).to receive(:run).with(*MASK_SPAWN_ARGS).and_return(result(IFCFG, 0))
+      expect(ip.netmask("eth0")).to eq("255.255.255.0")
+    end
+
+    it "returns nil when the command fails" do
+      expect(AwesomeSpawn).to receive(:run).with(*MASK_SPAWN_ARGS).and_return(result("", 1))
+      expect(ip.netmask("eth0")).to be_nil
+    end
+
+    it "returns nil if the netmask line is not present" do
+      bad_output = IFCFG.gsub(/netmask/, "")
+      expect(AwesomeSpawn).to receive(:run).with(*MASK_SPAWN_ARGS).and_return(result(bad_output, 0))
+      expect(ip.netmask("eth0")).to be_nil
     end
   end
 end
