e19975df0631666fe15da396f4043b0b76351406
Nick Carboni <ncarboni@redhat.com>
2015-10-07 17:19:31 -0400

    Added more spec tests around Common#cmd and Common#cmd?

--- spec/common_spec.rb
+++ spec/common_spec.rb
@@ -1,19 +1,37 @@
 describe LinuxAdmin::Common do
   subject { Class.new { include LinuxAdmin::Common }.new }
 
-  context "#cmd" do
+  describe "#cmd" do
     it "looks up local command from id" do
       expect(subject.cmd(:dd)).to match(/bin\/dd/)
     end
+
+    it "returns nil when it can't find the command" do
+      expect(subject.cmd(:kasgbdlcvjhals)).to be_nil
+    end
   end
 
-  it "#run" do
-    expect(AwesomeSpawn).to receive(:run).with("echo", nil => "test")
-    subject.run("echo", nil => "test")
+  describe "#cmd?" do
+    it "returns true when the command exists" do
+      expect(subject.cmd?(:dd)).to be true
+    end
+
+    it "returns false when the command doesn't exist" do
+      expect(subject.cmd?(:kasgbdlcvjhals)).to be false
+    end
   end
 
-  it "#run!" do
-    expect(AwesomeSpawn).to receive(:run!).with("echo", nil => "test")
-    subject.run!("echo", nil => "test")
+  describe "#run" do
+    it "runs a command with AwesomeSpawn.run" do
+      expect(AwesomeSpawn).to receive(:run).with("echo", nil => "test")
+      subject.run("echo", nil => "test")
+    end
+  end
+
+  describe "#run!" do
+    it "runs a command with AwesomeSpawn.run!" do
+      expect(AwesomeSpawn).to receive(:run!).with("echo", nil => "test")
+      subject.run!("echo", nil => "test")
+    end
   end
 end
