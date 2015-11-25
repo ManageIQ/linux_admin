502b66e656c2dd120cdb43a9d2fbe4f58ad382a1
Nick Carboni <ncarboni@redhat.com>
2015-11-25 11:30:10 -0500

    Added chrony class for editing chronyd time sync configuration

---
+++ lib/linux_admin/chrony.rb
@@ -0,0 +1,21 @@
+module LinuxAdmin
+  class Chrony
+    def initialize(conf = "/etc/chrony.conf")
+      raise MissingConfigurationFileError, "#{conf} does not exist" unless File.exist?(conf)
+      @conf = conf
+    end
+
+    def clear_servers
+      data = File.read(@conf)
+      data.gsub!(/^server\s+.+\n/, "")
+      File.write(@conf, data)
+    end
+
+    def add_servers(*servers)
+      data = File.read(@conf)
+      data << "\n" unless data.end_with?("\n")
+      servers.each { |s| data << "server #{s}\n" }
+      File.write(@conf, data)
+    end
+  end
+end
