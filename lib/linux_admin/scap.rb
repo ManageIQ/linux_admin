15f6593cf48378997037c15c21899015013187ba
Nick Carboni <ncarboni@redhat.com>
2017-09-28 16:31:24 -0400

    Alter the scap module to use the DataStream file over the XCCDF one
    Helps to address
    https://bugzilla.redhat.com/show_bug.cgi?id=1496279
    
    Also, some clarification can be found in https://bugzilla.redhat.com/show_bug.cgi?id=1496279#c10

--- lib/linux_admin/scap.rb
+++ lib/linux_admin/scap.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 
 module LinuxAdmin
   class Scap
-    PROFILE_ID = "linux-admin-scap"
+    PROFILE_ID = "xccdf_org.ssgproject.content_profile_linux-admin-scap".freeze
     SSG_XML_PATH = Pathname.new("/usr/share/xml/scap/ssg/content/")
 
     attr_reader :platform
@@ -14,36 +14,36 @@ module LinuxAdmin
       false
     end
 
-    def self.ssg_available?(platform = nil)
-      xccdf_file(platform) && oval_file(platform)
+    def self.ssg_available?(platform)
+      ds_file(platform).exist?
     end
 
-    def initialize(platform = nil)
+    def self.ds_file(platform)
+      SSG_XML_PATH.join("ssg-#{platform}-ds.xml")
+    end
+
+    def initialize(platform)
       @platform = platform
-    end
-
-    def ssg_available?
-      self.class.ssg_available?(platform)
     end
 
     def lockdown(*args)
       raise "OpenSCAP not available" unless self.class.openscap_available?
-      raise "SCAP Security Guide not available" unless ssg_available?
+      raise "SCAP Security Guide not available" unless self.class.ssg_available?(platform)
 
       values = args.last.kind_of?(Hash) ? args.pop : {}
       rules = args
 
       raise "No SCAP rules provided" if rules.empty?
 
-      with_xml_files(rules, values) do |xccdf_file_path|
-        lockdown_profile(xccdf_file_path, PROFILE_ID)
+      with_ds_file(rules, values) do |path|
+        lockdown_profile(path, PROFILE_ID)
       end
     end
 
-    def lockdown_profile(xccdf_file_path, profile_id)
+    def lockdown_profile(ds_path, profile_id)
       raise "OpenSCAP not available" unless self.class.openscap_available?
 
-      session = OpenSCAP::Xccdf::Session.new(xccdf_file_path)
+      session = OpenSCAP::Xccdf::Session.new(ds_path)
       session.load
       session.profile = profile_id
       session.evaluate
@@ -54,41 +54,12 @@ module LinuxAdmin
 
     private
 
-    def self.xccdf_file(platform)
-      local_ssg_file("xccdf", platform)
-    end
-
-    def self.oval_file(platform)
-      local_ssg_file("oval", platform)
-    end
-
-    def self.local_ssg_file(type, platform)
-      platform ||= "*"
-      Dir.glob(SSG_XML_PATH.join("ssg-#{platform}-#{type}.xml")).detect { |f| f =~ /ssg-\w+-#{type}.xml/ }
-    end
-
-    def tempdir
-      @tempdir ||= Pathname.new(Dir.tmpdir)
-    end
-
-    def xccdf_file
-      @xccdf_file ||= self.class.xccdf_file(platform)
-    end
-
-    def oval_file
-      @oval_file ||= self.class.oval_file(platform)
-    end
-
-    def with_xml_files(rules, values)
-      FileUtils.cp(oval_file, tempdir)
-
-      Tempfile.create("scap_xccdf") do |f|
-        write_xccdf_xml(f, profile_xml(PROFILE_ID, rules, values))
+    def with_ds_file(rules, values)
+      Tempfile.create("scap_ds") do |f|
+        write_ds_xml(f, profile_xml(PROFILE_ID, rules, values))
         f.close
         yield f.path
       end
-    ensure
-      FileUtils.rm_f(tempdir.join(File.basename(oval_file)))
     end
 
     def profile_xml(profile_id, rules, values)
@@ -103,13 +74,16 @@ module LinuxAdmin
       builder.doc.root.to_xml
     end
 
-    def write_xccdf_xml(io, profile_xml)
-      File.open(xccdf_file) do |f|
+    def write_ds_xml(io, profile_xml)
+      File.open(self.class.ds_file(platform)) do |f|
         doc = Nokogiri::XML(f)
-        model = doc.at_css("model")
-        model.add_next_sibling("\n#{profile_xml}")
+        model_xml_element(doc).add_next_sibling("\n#{profile_xml}")
         io.write(doc.root.to_xml)
       end
+    end
+
+    def model_xml_element(doc)
+      doc.css("//nist_list|model", "nist_list" => "http://checklists.nist.gov/xccdf/1.2").detect { |model| model.namespace.prefix.nil? }
     end
   end
 end
