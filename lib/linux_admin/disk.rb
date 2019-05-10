ec2ba4a0d56a58d35377fb6cf78ff7e37030ad00
Nick Carboni <ncarboni@redhat.com>
2019-05-10 15:00:10 -0400

    Support nvme device partition naming
    This commit collects the disk model from the parted output
    and uses that to change how we construct the partition path.
    
    Right now we keep the previous behavior of <device_path><partition_id>
    in all cases except nvme when we use <device_path>p<partition_id>.
    Previously, we would list a path which didn't exist for nvme devices.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1629853

--- lib/linux_admin/disk.rb
+++ lib/linux_admin/disk.rb
@@ -6,7 +6,7 @@ module LinuxAdmin
       [:id, :start_sector, :end_sector,
        :size, :partition_type, :fs_type]
 
-    attr_accessor :path
+    attr_accessor :path, :model
 
     def self.local
       result = Common.run!(Common.cmd("lsblk"), :params => {:d => nil, :n => nil, :p => nil, :o => "NAME"})
@@ -16,7 +16,8 @@ module LinuxAdmin
     end
 
     def initialize(args = {})
-      @path = args[:path]
+      @path  = args[:path]
+      @model = "unknown"
     end
 
     def size
@@ -101,6 +102,15 @@ module LinuxAdmin
       self
     end
 
+    def partition_path(id)
+      case model
+      when "nvme"
+        "#{path}p#{id}"
+      else
+        "#{path}#{id}"
+      end
+    end
+
     private
 
     def str_to_bytes(val, unit)
@@ -152,11 +162,17 @@ module LinuxAdmin
       out.each_line do |l|
         if l =~ /^ [0-9].*/
           split << l.split
+        elsif l =~ /^Model:.*/
+          parse_model(l)
         end
       end
       split
     end
 
+    def parse_model(parted_line)
+      matches = parted_line.match(/^Model:.*\((?<model>\w+)\)$/)
+      @model = matches[:model] if matches
+    end
 
     def partition_from_parted(output_disk)
       args = {:disk => self}
